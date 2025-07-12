daf1da2984d005e81b1461ed76ca93b8a3564815
LinkedOut <thelinkedoutofficial@gmail.com>
2025-07-12 05:52:46 +0530

    Update-5

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,6 @@ import Events from "@/components/Events";
 import CommunityPartners from "@/components/CommunityPartners";
 import Testimonials from "@/components/Testimonials";
 import JoinUs from "@/components/JoinUs";
-import Contact from "@/components/Contact";
 import Navigation from "@/components/Navigation";
 import FAQ from "@/components/FAQ";
 import LiquidBackground from "@/components/LiquidBackground";
